5df51fa8be0d39e1a6b25a149da7f9d99708c59c
elisandrof07 <f07nobru@gmail.com>
2025-02-09 14:45:53 +0100

    added general module

--- src/server/app/index.ts
+++ src/server/app/index.ts
@@ -1,7 +1,13 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { routes } from "../../infra/routes/routes";
 
 const app = Fastify({ logger: true });
+
+app.register(cors, {
+    origin: "*", // Permite requisições de qualquer origem
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+});
 
 app.register(routes);
 
